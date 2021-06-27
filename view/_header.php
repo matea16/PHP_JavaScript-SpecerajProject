426b36f0fd4af5e6a947d951aa9711549dd8edc3
mradenic <radenic.martina@gmail.com>
2021-06-27 10:53:15 +0200

    Dodan css

--- view/_header.php
+++ view/_header.php
@@ -5,19 +5,27 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
    <script type="text/javascript" src="javascript/naslovna.js"></script>
     <title>Špeceraj</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <canvas width="1700" height="300" id="cnv"></canvas>
 <ul>
       <li>
        <a href="index.php?rt=">Trgovine</a> 
+      </li>
+      <li>
        <a href="index.php?rt=">Proizvodi</a> 
+       </li>
+      <li>
        <a href="index.php?rt=">Pretraga</a> 
+       </li>
+      <li>
        <a href="index.php?rt=">Moja košarica</a>  
     </li>
-    <br>
-    <button id="btn-signin">Sign in</button>
+
+</ul>
+
+<button id="btn-signin">Sign in</button>
     <br>
     <br>
     <button id="btn-signup">Sign up</button>
-</ul>
