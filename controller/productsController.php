ae43856f7eefbeb0adfb27eb59b9d03cbf558190
mradenic <radenic.martina@gmail.com>
2021-07-10 14:56:38 +0200

    Dovrseno sortiranje

--- controller/productsController.php
+++ controller/productsController.php
@@ -20,6 +20,7 @@ class ProductsController{
             {
 
                 $looking  = $_POST['search'];
+                $keyWord = $_POST['search'];
                 $string = trim(preg_replace('!\s+!', ' ', $looking));
                 $key_words = explode(" ", $string);
                 $productList=SpecerajService::findProducts($key_words);
@@ -33,6 +34,34 @@ class ProductsController{
             }
         }
     
+        public function sortiraj()
+        {
+            $imeTrgovine  = $_GET['ime'];
+            $keyWord = $_GET['keyWord'];
+
+            $kako =  $_POST['kako'];
+
+            if($imeTrgovine !== "")
+            {
+                $idTrgovine = SpecerajService::getTrgovinaId($imeTrgovine);
+                $productList = SpecerajService::getProductsByStore($idTrgovine);
+
+            }
+
+            else if($keyWord !== "")
+            {
+                $looking  = $keyWord;
+                $string = trim(preg_replace('!\s+!', ' ', $looking));
+                $key_words = explode(" ", $string);
+                $productList=SpecerajService::findProducts($key_words);
+
+            }
+            else 
+                $productList=SpecerajService::getProductsOnAkcija();
+
+            $productList = SpecerajService::sortirajProizvode($productList, $kako);
+            require_once __DIR__.'/../view/products_index.php';
+        }
 };
 
 ?>
